69c679fbc656a47b42056ae0873b89bd277df22d
purewack <bobimaster15@gmail.com>
2023-03-29 18:52:05 +0100

    [dev] ⏰ sync issues WIP

--- src/sound.js
+++ src/sound.js
@@ -2,11 +2,11 @@ import * as Tone from "tone";
 import splKick from './sound/kick_soft.wav'
 import splSnare from './sound/snare_noise.wav'
 import splHat from './sound/hat8bit1.wav'
+import { PingPongDelay } from "tone";
 
 export function prepareSound (levelData,setInstruments,setCurrentPatterns,onGametick){
     Tone.start()
     Tone.Transport.bpm.value = levelData.music.bpm
-    Tone.Transport.start(0.2)
 
     const piano = new Tone.Synth().toDestination();
     piano.oscillator.type = "triangle";
@@ -14,7 +14,7 @@ export function prepareSound (levelData,setInstruments,setCurrentPatterns,onGame
 
     const bass = new Tone.Synth().toDestination();
     bass.oscillator.type = "pwm";
-    bass.volume.value = -20
+    bass.volume.value = -24
 
     const spl = new Tone.Sampler({
         urls: {
@@ -26,6 +26,7 @@ export function prepareSound (levelData,setInstruments,setCurrentPatterns,onGame
         console.log('samples loaded')
         }
     }).toDestination();
+    spl.volume.value = -20
 
     setInstruments({
         piano, 
@@ -33,11 +34,11 @@ export function prepareSound (levelData,setInstruments,setCurrentPatterns,onGame
         bass
     })
 
-    const when = Tone.Transport.nextSubdivision("1m");
-
     const pat = new Tone.Pattern((t,n)=>{
         spl.triggerAttackRelease(n,'8n',t)
     },levelData.music.beat.data)
+
+    const when = '1:0:0'
     pat.interval = levelData.music.beat.interval
     const tickId = Tone.Transport.scheduleRepeat((t)=>{
         Tone.Draw.schedule(()=>{
@@ -46,6 +47,13 @@ export function prepareSound (levelData,setInstruments,setCurrentPatterns,onGame
     }, levelData.gameTickInterval, when)
     pat.start(when)
     setCurrentPatterns({tick: tickId, beat:pat})
+
+    Tone.Transport.start(0.2)
+    Tone.Transport.position = '0:0:0'
+    console.log(when)
+    console.log(Tone.Transport.position)
+    console.log(Tone.Transport.state)
+    console.log(pat)
 }
 
 export function newBassLine (root, instrument, bassData, currentPatterns, setCurrentPatterns){
@@ -72,9 +80,10 @@ export function newBassLine (root, instrument, bassData, currentPatterns, setCur
             Tone.Time(bassData.interval).toSeconds()/2,
             t
         )
-    }, bassData.data).start('@1m')
+    }, bassData.data).start(QuanTime(Tone.Transport.position,4,4))
     p.interval = bassData.interval
     setCurrentPatterns({...currentPatterns, bass:p})
+    
   }
 
 export function endSound(currentPatterns,setCurrentPatterns){
@@ -83,3 +92,21 @@ export function endSound(currentPatterns,setCurrentPatterns){
     Tone.Transport.cancel()
     setCurrentPatterns(null)
 }
+
+export function QuanTime(nowTime, atBeats, barBeats){
+    const units = nowTime.split(':')
+    const nowBar = parseInt(units[0])
+    const nowBeat = parseInt(units[1])
+  
+    const quantize = (v,q)=>Math.floor((v + q)/q)*q;
+  
+    if(atBeats < barBeats){
+      const adv =  quantize(nowBeat,atBeats)
+      const nextBeat = adv%barBeats
+      const nextBar = nowBar + Math.floor(adv/barBeats)
+      return `${nextBar}:${nextBeat}:0`
+    }
+  
+    const adv = atBeats/barBeats
+    return `${quantize(nowBar,adv)}:0:0`
+  }
